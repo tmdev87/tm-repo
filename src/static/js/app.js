af69d080d6cd05f87ab81f4b9086770ec1c9fa74
tmdev87 <kookmin1987@gmail.com>
2024-08-14 12:37:29 +1000

    Updated add item string

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add'}
+                        {submitting ? 'Adding...' : 'Add item'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
